126443116959b099d041cb392f432595b17e40e4
henrique_vsj <henrique.vieira2008@hotmail.com>
2023-10-30 16:46:19 -0300

    Fix CodeQuality

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "roots": [
+        "<rootDir>/src"
+    ],
+    "testMatch": [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+    "transform": {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    "testResultsProcessor": "jest-sonar-reporter",
+    "collectCoverage": true,
+    "collectCoverageFrom": [
+        "**/*.{ts,js}",
+        "!**/node_modules/**"
+    ],
+    "coverageDirectory": "dist",
+    "coverageReporters": ["lcov"]
+};
